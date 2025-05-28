f60b408afd492321fcfaef5c7e2df25d5dcfa3af
Daniel Eddie Rollins <dan.rollins1@gmail.com>
2025-05-28 04:54:54 +0100

    last chance to get pre-commit working again

--- tests/test_io.py
+++ tests/test_io.py
@@ -7,6 +7,7 @@ from unittest.mock import patch
 import numpy as np
 import pytest
 from PIL import Image, ImageSequence
+
 from playNano.io.gif_export import (
     create_gif_with_scale_and_timestamp,
     normalize_to_uint8,
