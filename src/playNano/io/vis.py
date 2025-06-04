07b41a6a881b15af29c1d7665dd746c89f3875e2
Daniel Eddie Rollins <dan.rollins1@gmail.com>
2025-06-04 02:25:34 +0100

    fix linting

--- src/playNano/io/vis.py
+++ src/playNano/io/vis.py
@@ -348,9 +348,10 @@ def play_stack_cv(
             bar_length_nm=scale_bar_nm,
         )
 
-        canvas[y_off : y_off + disp_size[1], x_off : x_off + disp_size[0]] = (  # noqa
-            resized
-        )
+        canvas[
+            y_off : y_off + disp_size[1], x_off : x_off + disp_size[0]  # noqa
+            ] = resized
+        
 
         cv2.imshow(window_name, canvas)
 
