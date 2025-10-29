07dfc16aa56590bce2d121cc385977493bb7cf76
Daniel Eddie Rollins <dan.rollins1@gmail.com>
2025-10-29 20:19:27 +0000

    Add backward compatibility shims for playNano -> playnano

---
+++ src/playnano/playNano/analysis/__init__.py
@@ -0,0 +1,11 @@
+"""Backward compatibility shim and warning"""
+
+import warnings
+
+warnings.warn(
+    "Importing 'playNano' (mixed case) is deprecated and will be removed in a future release. "
+    "Please import 'playnano' (lowercase) instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+from playnano.analysis import *  # noqa: F401,F403,E402
